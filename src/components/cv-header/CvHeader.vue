8dfa2c3d4966e19879eae7bd37e38a3ee019de3d
Guillermo Gallay <guillermogallay@gmail.com>
2020-10-19 21:16:46 -0300

    feat(ui): create CvHeader component

---
+++ src/components/cv-header/CvHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-card 
+        tile
+        color="black">
+        <v-card-title>
+            <v-row align="center">
+                <v-col 
+                    sm="5"             
+                    cols="12"
+                    align="center"> 
+                    <v-img
+                        alt="Foto de Perfil" 
+                        width="100px" 
+                        src="@/assets/profilePicture.png">
+                    </v-img>
+                </v-col>
+                <v-col
+                    sm="6"
+                    cols="12">
+                    <v-row justify="center">
+                        <h2 class="white--text">Guillermo Gallay</h2>
+                    </v-row>
+                    <v-row justify="center">
+                        <h4 class="white--text">FrontEnd Developer</h4>
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-card-title>
+    </v-card>
+</template>
+
