4e80dd59b5fdce23f701273c895b97eeafddd1c2
Guillermo Gallay <guillermogallay@gmail.com>
2020-10-19 21:19:24 -0300

    feat(ui): create CvDigital view

---
+++ src/views/cv-digital/CvDigital.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container 
+    fluid
+    class="d-flex justify-center">
+      <v-col
+        cols="12" 
+        md="8">
+          <v-card 
+            color="white" 
+            tile>
+              <CvHeader />
+          </v-card>
+      </v-col>
+  </v-container>
+</template>
+
+<script>
+  import CvHeader from "@/components/cv-header/CvHeader.vue";
+
+  export default {
+    components: {
+    CvHeader,
+    },
+  }
+</script>
+
