3534275b26a5735f287b2dee125da342bdf3d9c5
Guillermo Gallay <guillermogallay@gmail.com>
2022-02-20 12:22:22 -0300

    fix(ui): add SideBar component

---
+++ src/components/sideBar/SideBar.js
@@ -0,0 +1,81 @@
+export default {
+    data () {
+        return {
+            contactos: [
+                {
+                name:"mdi-linkedin",
+                link:"https://www.linkedin.com/in/guillermo-augusto-gallay-913808185/",
+                description:"Guillermo Gallay",
+                },
+                {
+                name:"mdi-email",
+                link:"mailto:guillermogallay@gmail.com",
+                description: "guillermogallay@gmail.com",
+                },
+                {
+                name:"mdi-phone",
+                link:"tel:+5493442645967",
+                description:"+5493442645967",
+                },
+                {
+                name:"mdi-github",
+                link:"https://github.com/guillermogallay",
+                description:"guillermogallay",
+                }
+            ],
+            herramientas: [
+                {
+                    id: 1,   
+                    name: "Git",
+                    value: 3
+                },
+                {
+                    id: 2,
+                    name: "JavaScript",
+                    value: 4
+                },
+                {
+                    id: 3,
+                    name: "HTML",
+                    value: 4
+                },
+                {
+                    id: 4,
+                    name: "CSS",
+                    value: 4
+                },
+                {
+                    id: 5,
+                    name: "Vuetify",
+                    value: 4
+                },
+                {
+                    id: 6,
+                    name: "Vue.js",
+                    value: 4
+                },
+                {
+                    id: 7,
+                    name: "C#.Net",
+                    value: 4
+                },
+                {
+                    id: 8,
+                    name: "Angular 11",
+                    value: 4
+                },
+                {
+                    id: 9,
+                    name: "TypeScript",
+                    value: 4
+                },
+                {
+                    id: 10,
+                    name: "Bootstrap 4",
+                    value: 4
+                },
+                
+            ]
+        }
+    },
+}
---
+++ src/components/sideBar/SideBar.vue
@@ -0,0 +1,74 @@
+<template>
+    <v-col
+        cols="12"
+        xs="12"
+        sm="5" 
+        class="text-justify"
+        align="center">
+        <section>
+            <v-card-title>
+                <h4 roboto>SOBRE MÍ</h4>
+            </v-card-title>
+            <v-card-text>
+                <p>
+                    Soy una persona muy responsable, puntual y aplicada que 
+                    le gusta mucho el desarrollo web, sobre todo el frontend.
+                </p>
+                <p>
+                    Trato de aprovechar al máximo el trabajo en equipo, 
+                    siempre estoy abierto a evaluar diferentes puntos de vista 
+                    y opiniones con el fin de alcanzar el mejor resultado.
+                </p>
+                <p>
+                    Me considero una persona autodidacta, cuando se presenta algún problema trato de 
+                    resolverlo aplicando mis experiencias o investigando para adquirir nuevos conocimientos.
+                </p> 
+                <p>
+                    Mi principal objetivo es seguir mejorando día a día como recurso, 
+                    tanto en lo técnico como personal.
+                </p> 
+            </v-card-text>
+        </section>
+        <section>
+            <v-card-title>
+                <h4 roboto>CONTACTO</h4>
+            </v-card-title>
+            <v-card-text>
+                <p>
+                    <a 
+                        v-for="contacto in contactos"
+                        :key="contacto.name"
+                        :href="contacto.link"
+                        target="_blank"
+                        class="text-decoration-none"> 
+                        <v-row>
+                            <v-col>
+                                <v-icon 
+                                    color="#212529">
+                                    {{contacto.name}}
+                                </v-icon>
+                                {{contacto.description}}
+                            </v-col>
+                        </v-row>
+                    </a> 
+                </p>
+            </v-card-text>
+        </section>
+    
+        <section>
+            <v-card-title>
+                <h4 roboto>HERRAMIENTAS</h4>
+            </v-card-title>
+            <v-card-text >
+                <v-chip 
+                    class="ma-2"
+                    v-for="herramienta in herramientas"
+                    :key="herramienta.id">
+                        {{herramienta.name}}
+                </v-chip>
+            </v-card-text>
+        </section>
+    </v-col>
+</template>
+
+<script src="./SideBar.js"></script>
