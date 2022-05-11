11362ce866ea1d16af37b8dfd4dae4b38fa049c3
Guillermo Gallay <guillermogallay@gmail.com>
2022-05-11 19:59:13 -0300

    refacto(ui): create content  component

---
+++ src/components/content/Content.vue
@@ -0,0 +1,130 @@
+<template>
+    <div>
+
+        <section>
+            <v-card-title>
+                <v-icon 
+                    color="#212529" 
+                    class="mr-2">
+                        mdi-briefcase-variant
+                </v-icon>
+                <h4 roboto>EMPLEOS</h4>
+            </v-card-title>
+            <v-card-text>
+                <section>
+                    <h2>Soporte Técnico al Cliente </h2>
+                    <p>Enero 2020 - Noviembre 2020 - Sidesys</p>
+                </section>
+                <section>
+                    <h2>Front-End Developer Vue.js</h2>
+                    <p>Agosto 2020 - Actualidad - Freelance</p>
+                </section>
+                <section>
+                    <h2>Front-End Developer Angular 11</h2>
+                    <p>Diciembre 2020 - Actualidad - Sidesys</p>
+                </section>
+            </v-card-text>
+        </section>
+        <section>
+            <v-card-title>
+                <v-icon 
+                    color="#212529" 
+                    class="mr-2">
+                        mdi-head-cog
+                </v-icon>
+                <h4 roboto>EXPERIENCIAS</h4>
+            </v-card-title>
+            <v-card-text>
+                <section>
+                    <h2>Curso de C#.Net</h2>
+                    <p>2019 - Taller de programacion - C# .Net</p>
+                </section>
+                <section>
+                    <h2>Desarrollo de CV-Digital Responsive</h2>
+                    <p>Actualidad -  Proyecto personal - Vue.js - Vuetify </p>
+                </section>
+                <section>
+                    <h2>Desarrollo de eCommerce</h2>
+                    <p class="parrafo">Actualidad -  Proyecto personal - Vue.js - Vuetify</p>
+                    <p>Proyecto personal en el cual desarrollamos un e-comerse. En el mismo me desempeño 
+                        como desarrollador FrontEnd. Cuenta con dos aplicaciones FrontEnd, 
+                        la que accede el cliente del comercio y la que usa el dueño para la administracion.
+                        Se diseño para que sea utilizada tambien en resolucion de celular.
+                    </p>
+                </section>
+                <section>
+                    <h3>Front-End Developer Angular 11+</h3>
+                    <p class="parrafo">Diciembre 2020 - Actualidad - Sidesys</p>
+                    <p>
+                        Mi labor es diseñar y desarrollar aplicaciones Angular escalables. No solo desarrolo, si no tambien diseño UI/UX. 
+                        En su mayoria los proyectos son de reporteria y presentacion de datos.
+                    </p>
+                </section>
+            </v-card-text>
+        </section>
+        <section>
+            <v-card-title>
+                <v-icon 
+                    color="#212529" 
+                    class="mr-2">
+                        mdi-school
+                </v-icon>
+                <h4 roboto>ESTUDIOS</h4>
+            </v-card-title>
+            <v-card-text>
+                <section>
+                    <h3>Título Escuela Secundaria</h3>
+                    <p class="parrafo">2010 - Escuela Secundaria N°2</p>
+                </section>
+                <section>
+                    <h3>Ingeniería en Sistemas de Información</h3>
+                    <p class="parrafo">Actualidad - UTN - FRCU</p>
+                </section>
+                <section>
+                    <h3>Aprende Typescript de 0 a 100 </h3>
+                    <p class="parrafo">
+                        <a target="_blank"
+                            class="text-decoration-none"
+                            href="https://udemy-certificate.s3.amazonaws.com/pdf/UC-25d49b37-297e-4922-8dbb-fe6998e2edea.pdf">(Certificado)</a>
+                    </p>
+                </section>
+                <section>
+                    <h3>Scrum Fundation Professional Certificate SFPC</h3>
+                    <p class="parrafo">
+                        <a target="_blank"
+                            class="text-decoration-none"
+                            href="https://certificates.easy-lms.com/exam/session/df671574-a188-45fd-9aca-ab5e30cf46d4">(Certificado)</a>
+                    </p>
+                </section>
+                <section>
+                    <h3>Fundamentos de Angular</h3>
+                    <p class="parrafo">
+                        <a target="_blank"
+                            class="text-decoration-none"
+                            href="https://udemy-certificate.s3.amazonaws.com/pdf/UC-026e8ac9-146f-43c8-bedc-6bc6e75ad947.pdf">(Certificado)</a>
+                    </p>
+                </section>
+                <section>
+                    <h3>Curso Profesional de JavaScript</h3>
+                    <p class="parrafo">
+                        <a target="_blank"
+                            class="text-decoration-none"
+                            href="https://drive.google.com/file/d/1_jgAIOrYAi7MBNygOhWP_k8-ndBTz8_7/view?usp=sharing">(Certificado)</a>
+                    </p>
+                </section>
+                <section>
+                    <h3>Angular para principiantes: Crea una aplicación real</h3>
+                        <p class="parrafo">
+                        <a target="_blank"
+                            class="text-decoration-none"
+                            href="https://www.udemy.com/course/angular-principiantes-leifer-mendez/">(En Curso)</a>
+                    </p>
+                </section>
+            </v-card-text>
+        </section>
+    </div>
+</template>
+
+<script src="./content.js"></script>
+
+<style src="./content.css" scoped></style>
---
+++ src/components/content/content.js
@@ -0,0 +1,83 @@
+export default {
+    data () {
+        return {
+            contactos: [
+                {
+                name:"mdi-linkedin",
+                link:"https://www.linkedin.com/in/guillermo-augusto-gallay-913808185/",
+                description:"Guillermo Gallay",
+                },
+                {
+                name:"mdi-email",
+                link:"mailto:guillermogallay@gmail.com",
+                description: "guillermogallay@gmail.com",
+                },
+                {
+                name:"mdi-phone",
+                link:"tel:+5493442645967",
+                description:"+5493442645967",
+                },
+                {
+                name:"mdi-github",
+                link:"https://github.com/guillermogallay",
+                description:"guillermogallay",
+                }
+            ],
+            herramientas: [
+                {
+                    id: 1,   
+                    name: "Git"
+                },
+                {
+                    id: 2,
+                    name: "JavaScript"
+                },
+                {
+                    id: 3,
+                    name: "HTML"
+                },
+                {
+                    id: 4,
+                    name: "CSS"
+                },
+                {
+                    id: 5,
+                    name: "Vuetify"
+                },
+                {
+                    id: 6,
+                    name: "Vue.js"
+                },
+                {
+                    id: 7,
+                    name: "C#.Net"
+                },
+                {
+                    id: 8,
+                    name: "Angular 11"
+                },
+                {
+                    id: 9,
+                    name: "TypeScript"
+                },
+                {
+                    id: 10,
+                    name: "Bootstrap 4"
+                },
+                {
+                    id: 11,
+                    name: "Jasmin y Karma"
+                },
+                {
+                    id: 12,
+                    name: "PrimeNG"
+                },
+                {
+                    id: 13,
+                    name: "RxJs"
+                },
+                
+            ]
+        }
+    },
+}
