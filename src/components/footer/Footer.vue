0ac8278ef9aed554418494c72b15226895f84888
Guillermo Gallay <guillermogallay@gmail.com>
2022-05-11 19:59:33 -0300

    refacto(ui): create footer  component

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-footer
+        color="black"
+        class="text-center">
+          <v-card-text>
+            <a 
+                v-for="icon in icons"
+                :key="icon.id"
+                :href="icon.link"
+                target="_blank"
+                class="ma-2 text-decoration-none"> 
+                    <v-icon color="white">{{icon.name}}</v-icon>
+            </a> 
+          </v-card-text>
+
+          <v-card-text class="white--text">
+            {{ dateNow }} — By Guillermo Gallay
+          </v-card-text>
+    </v-footer>
+</template>
+
+<script src="./footer.js"></script>
---
+++ src/components/footer/footer.js
@@ -0,0 +1,31 @@
+
+export default {
+
+    data () {
+        return {
+            icons: [
+                {
+                    id: 1,
+                    name:"mdi-linkedin",
+                    link:"https://www.linkedin.com/in/guillermo-augusto-gallay-913808185/",
+                },
+                {
+                    id: 2,
+                    name:"mdi-email",
+                    link:"mailto:guillermogallay@gmail.com",
+                },
+                {
+                    id: 3,
+                    name:"mdi-phone",
+                    link:"tel:+5493442645967",
+                },
+                {
+                    id: 4,
+                    name:"mdi-github",
+                    link:"https://github.com/guillermogallay",
+                }
+            ],
+            dateNow: new Date().getFullYear(),   
+        }
+    },
+  }
